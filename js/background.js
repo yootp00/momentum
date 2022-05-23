989c8930dad1cd48d669597eafae9f4b67d83cd9
yootp00 <yootp00@naver.com>
2022-05-23 23:51:08 +0900

    backgroundchange

--- js/background.js
+++ js/background.js
@@ -2,10 +2,11 @@ const images = ["0.jpg", "1.png", "2.png", "3.jpg","4.png",
  "5.png", "6.png", "7.jpg", "8.png",
  "9.jpg", "10.jpg", "11.jpg", "12.png", "13.jpg", "14.jpg"];
 
- const chosenImages =images[Math.floor(Math.random() * images.length)];
+const chosenImages =images[Math.floor(Math.random() * images.length)];
 
- const bgImage = document.createElement("img");
+function bgImage(){
+    const url = "url('img/"+chosenImages+"')";
+    document.body.style.backgroundImage = url;
+}
 
- bgImage.src=`img/${chosenImages}`;
-
- document.body.appendChild(bgImage);
+bgImage();
